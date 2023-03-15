d0e306a582508b9f36e4bf2e9df29933ec021064
byeong_chang <43203949+byeong-chang@users.noreply.github.com>
2023-03-15 15:01:37 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/README.md
+++ 프로그래머스/lv0/120885. 이진수 더하기/README.md
@@ -1,6 +1,6 @@
 # [level 0] 이진수 더하기 - 120885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120885#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120885) 
 
 ### 성능 요약
 
--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.py
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.py
@@ -1,24 +1,21 @@
 def solution(bin1, bin2):
-    diff = abs(len(bin1) - len(bin2))
-    if len(bin1) > len(bin2):
-        bin2 = 0*diff + bin2
-    elif len(bin1) < len(bin2):
-        bin1 = 0*diff + bin1
+    temp = 1
+    bin_1 = 0
+    bin_2 = 0
     answer = []
-    head ,temp= 0,0
-    for i in range(len(bin1)-1 , -1 ,-1):
-        temp = head + int(bin1[i]) + int(bin2[i])
-        if temp == 0:
-            answer.append("0")
-            head = 0
-        elif temp == 2:
-            answer.append("0")
-            head = 1
-        else:
-            head = temp//2
-            answer.append(str(temp%2))
-    if head == 1:
-        answer.append(str("1"))
+    for i in range(len(bin1)-1,-1,-1):
+        bin_1 += temp*int(bin1[i])
+        temp *=2
+    temp = 1
+    for i in range(len(bin2)-1,-1,-1):
+        bin_2 += temp*int(bin2[i])
+        temp *=2
+    total = bin_1 + bin_2
+    print(total)
+    while True:
+        answer.append(str(total%2))
+        total//=2
+        if total == 0:
+            break
     answer.reverse()
-    answer =  "".join(answer)
-    return answer
+    return "".join(answer)
