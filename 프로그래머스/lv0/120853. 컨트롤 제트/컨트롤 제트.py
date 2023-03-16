f4ce9a7236bf61ac9df20b8cbce975ffc1fa1874
byeong_chang <43203949+byeong-chang@users.noreply.github.com>
2023-03-16 11:04:59 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = 0
+    s = s.split()
+    print(s)
+    for i in range(len(s)):
+        if s[i] == 'Z':
+            answer -= int(s[i-1])
+        else: answer += int(s[i])
+            
+    return answer
