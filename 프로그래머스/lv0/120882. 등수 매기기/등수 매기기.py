c0f6ea2f7f4da8e0428051ca09b85fae9239f610
byeong_chang <43203949+byeong-chang@users.noreply.github.com>
2023-03-17 15:09:51 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
@@ -1,7 +1,6 @@
 def solution(score):
     score = list(map(sum,score))
-    temp = score.copy()
-    temp.sort(reverse = True)
+    temp = sorted(score,reverse = True)
     answer = []
     
     for val in score:
