1e88d199d5d650ad9c5e2242c03510ac573bb261
byeong_chang <43203949+byeong-chang@users.noreply.github.com>
2023-03-28 13:58:01 +0900

    [level 2] Title: 구명보트, Time: 11.68 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,15 @@
+from collections import deque
+def solution(people, limit):
+    
+    answer = 0
+    people = deque(sorted(people))
+    while len(people)>1:
+        answer += 1
+        if people[0] + people[-1] <= limit:
+            people.pop()
+            people.popleft()
+        else: 
+            people.pop()
+    
+    if people:answer+=1
+    return answer
