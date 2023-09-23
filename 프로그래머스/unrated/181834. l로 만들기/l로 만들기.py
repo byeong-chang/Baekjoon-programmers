82c1a3723d57cfed61e01ed0807bde542ff9cd3f
byeong_chang <43203949+byeong-chang@users.noreply.github.com>
2023-09-24 02:08:01 +0900

    [unrated] Title: l로 만들기, Time: 1.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,2 @@
+def solution(myString):
+    return myString.replace('a','l').replace('b','l').replace('c','l').replace('d','l').replace('e','l').replace('f','l').replace('g','l').replace('h','l').replace('i','l').replace('j','l').replace('k','l')
