1ce1ac044215a94d0b7fbe003de27bc05b025d97
byeong_chang <43203949+byeong-chang@users.noreply.github.com>
2023-09-25 18:04:45 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,6 @@
+def solution(my_string):
+    answer = [0 for i in range(52)]
+    for word in my_string:
+        if word.isupper():answer[ord(word)-65]+=1
+        else: answer[ord(word) - 71]+=1
+    return answer
