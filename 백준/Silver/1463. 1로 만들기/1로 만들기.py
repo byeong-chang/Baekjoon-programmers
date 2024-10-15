d17523a92237feb101fd51f12e8069bece7b35ed
byeong-chang <43203949+byeong-chang@users.noreply.github.com>
2024-10-15 15:03:13 +0900

    [Silver III] Title: 1로 만들기, Time: 592 ms, Memory: 39368 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,23 +1,17 @@
 import sys
-x = int(sys.stdin.readline())
 
-dp_x = [0,0]
-def factor_third(num):
-    if num %3==0:
-        return dp_x[num//3] + 1
-    else : 
-        return dp_x[num-1]+1
+def input():
+    return sys.stdin.readline().rstrip()
+    
 
-def factor_second(num):
-    if num %2 == 0:
-        return dp_x[num//2] + 1 
-    else: 
-        return dp_x[num-1]+1    
-
-def minus_one(num):
-    return dp_x[num-1]+1
-
-for i in range(2,x+1):
-    dp_x.append(min(factor_second(i),factor_third(i),minus_one(i)))
-
-print(dp_x[x])
+n = int(input())
+dp = [0,0,1,1] 
+for i in range(4,n+1):
+    dps = []
+    if i % 3 == 0:
+        dps.append(dp[i//3])
+    if i % 2 == 0:
+        dps.append(dp[i//2])
+    dps.append(dp[i-1])
+    dp.append(min(dps) + 1)
+print(dp[n])
