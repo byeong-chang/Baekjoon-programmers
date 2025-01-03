6ed735b5566265a55eb6a57873d5a5be7170180b
byeong-chang <43203949+byeong-chang@users.noreply.github.com>
2025-01-03 15:50:19 +0900

    [Silver III] Title: N과 M (8), Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,25 @@
+'''
+중복조합 출력문제
+'''
+
+import sys
+
+def input():
+    return sys.stdin.readline()
+
+N,M = map(int,input().split())  
+Ns = sorted(list(map(int,input().split())))
+select = [0 for _ in range(M)]
+
+def comb(idx,sidx):
+    if sidx == M:
+        print(*select)
+        return
+    if idx == N:
+        return
+    
+    select[sidx] = Ns[idx]
+    comb(idx, sidx+1)
+    comb(idx+1, sidx)
+
+comb(0,0)
