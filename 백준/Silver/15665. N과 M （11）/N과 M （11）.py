e66d7d90761a4d89b92513a44d83600c6ce3ca8b
byeong-chang <43203949+byeong-chang@users.noreply.github.com>
2025-02-15 16:19:09 +0900

    [Silver II] Title: N과 M (11), Time: 2024 ms, Memory: 242772 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,25 @@
+
+import sys
+from collections import deque
+def input():
+    return sys.stdin.readline().rstrip()
+
+n,m = map(int,input().split())
+lst = list(map(int,input().split()))
+sel = [0 for _ in range(m)]
+answer = []
+
+def perm(depth):
+    if depth == m:
+        answer.append(sel[::])
+        return
+
+    for i in range(n):
+        sel[depth] = lst[i]
+        perm(depth+1)
+
+
+perm(0)
+
+for val in sorted(list(set([tuple(v) for v in answer]))):
+    print(*val)
