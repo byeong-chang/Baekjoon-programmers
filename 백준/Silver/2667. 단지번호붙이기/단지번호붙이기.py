3f8e3c7675af9442b7bfc47f5971d808f14949f9
byeong-chang <43203949+byeong-chang@users.noreply.github.com>
2025-02-21 15:22:43 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,36 +1,37 @@
 import sys
 from collections import deque
-N = int(sys.stdin.readline())
-graph = [list(map(int,sys.stdin.readline().strip()))for i in range(N)]
-visited = [[False]*N for i in range(N)]
-count = 0
-count_lst = []
+def input():
+    return sys.stdin.readline().rstrip()
+# map(int,input().split())
+
+n = int(input())
+lst = [list(map(int,input())) for _ in range(n)]
+visited = [[False] * n  for _ in range(n)]
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
+answer = []
 
-def BFS(a,b):
-    Q= deque()
-    Q.append([a,b])
-    cnt = 0
-    while Q:
-        cnt+=1
-        x,y = Q.popleft()
+def dfs(stack):
+    count = 0
+    while stack:
+        x,y = stack.pop()
+        count+=1
         for i in range(4):
-            nx,ny = x+dx[i] , y+dy[i]
-            if not(nx<=-1 or ny<=-1 or nx>=N or ny>=N):
-                if visited[nx][ny] == False and graph[nx][ny] == 1:
-                    Q.append([nx,ny])
-                    visited[nx][ny] = True
-    return cnt
+            ddx = x + dx[i]
+            ddy = y + dy[i]
+            if (ddx < n and ddy < n and ddx > -1 and ddy > -1) and lst[ddx][ddy]and not visited[ddx][ddy]:
+                visited[ddx][ddy] = True
+                stack.append([ddx,ddy])
+    return count
 
-for x in range(N):
-    for y in range(N):
-        if visited[x][y] == False and graph[x][y] == 1:
-            visited[x][y] = True
-            count_lst.append(BFS(x,y))
-            count+=1
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j] and lst[i][j]:
+            visited[i][j] = True
+            temp = dfs([[i,j]])
+            if temp:
+                answer.append(temp)
 
-count_lst.sort()
-print(count)
-for i in count_lst:
-    print(i)
+print(len(answer))
+for v in sorted(answer):
+    print(v)
